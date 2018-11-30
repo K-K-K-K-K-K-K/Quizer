15caf05fc00771fc5050753234b7f3f753b71e9a
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-11-30 10:21:03 +0900

    あ

--- Main.c
+++ Main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 void main() {
+	int sel;
+	scanf("%d", &sel);
+
+	switch(sel) {
+		case 1:
+			exit(0);
+			break;
+
+		default:
+			printf("[エラー] 不正な入力\n");
+	}
+
+	main();
 }
 
