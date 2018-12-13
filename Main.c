96dbb55f022af479e85426e4f8720e4b25487be1
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-12-13 09:36:23 +0900

    toriaezu

--- Main.c
+++ Main.c
@@ -8,7 +8,6 @@ void printHelpMsg();
 void main() {
 	printStartMsg();
 	printHelpMsg();
-	printf("\n");
 
 	int sel;
 	for (;;) {
@@ -22,17 +21,16 @@ void main() {
 
 			case 2:
 				printHelpMsg();
-				printf("\n\n");
 				break;
 
 			case 3:
 				printf("KANJHI\n");
-				printf("\n\n");
+				printf("\n");
 				break;
 
 			default:
 				printf("[エラー] 不正な入力\n");
-				printf("\n\n");
+				printf("\n");
 		}
 	}
 }
@@ -44,8 +42,9 @@ void printStartMsg() {
 
 void printHelpMsg() {
 	printf("コマンド一覧表\n");
-	printf("\t1: 終了\n");
-	printf("\t2: コマンド一覧表の表示\n");
-	printf("\t3: [Quiz] 漢字の読み方\n");
+	printf("    1: 終了\n");
+	printf("    2: コマンド一覧表の表示\n");
+	printf("    3: [Quiz] 漢字の読み方\n");
+	printf("    \n");
 }
 
