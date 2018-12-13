5567dceb0ff1a7050f09eea62a4605d19ede66a7
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2018-12-13 09:34:45 +0900

    toriaezu

--- Main.c
+++ Main.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Quiz.h"
+
+void printStartMsg();
+void printHelpMsg();
 
 void main() {
+	printStartMsg();
+	printHelpMsg();
+	printf("\n");
+
 	int sel;
-	scanf("%d", &sel);
+	for (;;) {
+		printf("> ");
+		scanf("%d", &sel);
 
-	switch(sel) {
-		case 1:
-			exit(0);
-			break;
+		switch(sel) {
+			case 1:
+				exit(0);
+				break;
 
-		default:
-			printf("[エラー] 不正な入力\n");
+			case 2:
+				printHelpMsg();
+				printf("\n\n");
+				break;
+
+			case 3:
+				printf("KANJHI\n");
+				printf("\n\n");
+				break;
+
+			default:
+				printf("[エラー] 不正な入力\n");
+				printf("\n\n");
+		}
 	}
+}
 
-	main();
+void printStartMsg() {
+	printf("Quizer 第0.1版\n");
+	printf("\n");
+}
+
+void printHelpMsg() {
+	printf("コマンド一覧表\n");
+	printf("\t1: 終了\n");
+	printf("\t2: コマンド一覧表の表示\n");
+	printf("\t3: [Quiz] 漢字の読み方\n");
 }
 
