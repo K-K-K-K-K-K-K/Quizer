6df4c764d675fc752ab4f93cb9c862a7ba655c70
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2019-01-18 09:11:55 +0900

    a

---
+++ QAFileReader.c
@@ -0,0 +1,18 @@
+QAUnit *getQAs(char *path) {
+	QAUnit *qas = malloc(sizeof(QAUnit * ASIZE);
+
+	// ファイル開く
+
+	for (/*ファイルの終わりまで*/) {
+		// 一行読み込む
+		qas[i] = getQA(line);
+	}
+
+	return qas;
+}
+
+QAUnit getQA(char *s) {
+	// 文字列を分解し、QAUnitに代入し返却
+	return;
+}
+
